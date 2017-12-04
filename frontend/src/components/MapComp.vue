afd2088cb94c486e7063016b2fe57888046bf192
JASON <jaeseonglee@umass.edu>
2017-12-04 00:10:13 -0500

    relocate components

---
+++ frontend/src/components/MapComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mapcomp">
+    <v-map :zoom=13 :center="[42.38681,-72.52547]">
+      <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+      <!-- <v-marker :lat-lng="[42.38681,-72.52547]"></v-marker> -->
+    </v-map>
+  </div>
+</template>
+<script>
+export default {
+  name: 'mapcomp'
+}
+</script>
+<style scoped>
+.mapcomp {
+  position: absolute;
+  left: 35%;
+  width: 65%;
+  top: 0; bottom: 0;
+}
+</style>
