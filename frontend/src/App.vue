2d15bdcf032d83d44e696a3da10b8cf64c703089
JASON <jaeseonglee@umass.edu>
2017-12-09 10:11:20 -0500

    -

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <sidebar></sidebar>
     <mapcomp></mapcomp>
-
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import MapComp from './components/MapComp'
-import SideBar from './components/SideBar'
 Vue.component('mapcomp', MapComp)
-Vue.component('sidebar', SideBar)
-
 export default {
   name: 'app',
   components: {
-    MapComp, SideBar
+    MapComp
   }
 }
 </script>
