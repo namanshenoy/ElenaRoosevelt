47fda7ca8f5b80af30c87da46698ce9996325ddb
Corey Collins <coreycwcollins@gmail.com>
2017-12-11 23:57:56 -0500

    D3 Chart and Shortest path API request and radio button

--- backend/static/script.js
+++ backend/static/script.js
@@ -283,6 +283,11 @@ function goBack(){
   }
 }
 
+function forceLoad(){
+  console.log("CLICKED")
+  document.getElementById("page-load").style.display="none";
+}
+
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
